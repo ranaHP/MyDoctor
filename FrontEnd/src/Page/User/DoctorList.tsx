15b2c99968c4700dd115253bf985e87416df0705
R_Hansana <hansana.ranaweera@taprotechnologies.com>
2024-07-13 13:17:50 +0530

    INT | Add axios to routing purposes

--- FrontEnd/src/Page/User/DoctorList.tsx
+++ FrontEnd/src/Page/User/DoctorList.tsx
@@ -1,16 +1,40 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
+interface User {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+  }
+
+  
 const DoctorList = () => {
+    const [users, setUsers] = useState<User[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+  
+    useEffect(() => {
+      axios.get<User[]>('https://jsonplaceholder.typicode.com/users')
+        .then(response => {
+          setUsers(response.data);
+          setLoading(false);
+        })
+        .catch(error => {
+          setError(error.message);
+          setLoading(false);
+        });
+    }, []);
+  
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error}</p>;
   return (
     <div className='w-full flex'>
-        {[
-            'docor1',
-            'docor2',
-            'docor3',
-            'docor4'
-        ].map((item, index) => (
-            <div className='h-[100px] w-[100px] bg-slate-200 rounded-lg m-10 flex justify-center items-center' key={index}>
-                <div>{item}</div>
+        {users.map((item, index) => (
+            <div className='h-[200px] w-[300px] bg-slate-50 rounded-lg m-10 flex flex-col flex-nowrap items-center justify-center' key={index}>
+                <div>{item.name}</div>
+                <div>{item.email}</div>
+                <div>{item.username}</div>
             </div>
         ))}  
 
